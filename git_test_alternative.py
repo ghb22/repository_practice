df755372beb21b18b2162fe702b222954d1db4c6
ghb22 <george.h.beane@gmail.com>
2018-12-23 11:30:15 +0000

    adding all files Dec 23

---
+++ git_test_alternative.py
@@ -0,0 +1,46 @@
+for x in range (20):
+    print (x)
+
+for x in range (40):
+    print (x)
+
+
+#a repository = a project in Git
+
+#COMMANDS
+
+#git
+# this shows all commands
+
+#cd --> define where you want to project to live
+#copy the url from the website
+#git clone https://github.com/ghb22/dec_practice_2.git // pulls the project i.e. repository onto your computer
+
+# git status
+#shows what things you're working on your computer and whether they've been uploaded
+
+# git add [name of file here]
+#this adds your file to the project
+
+# git commit -m "m is used to show a message which this is"
+# this queus it up so it's ready to load on the project
+
+#git push
+# this uploads to your repository
+
+#git pull
+#this brings in new files to your repository
+
+#if your partner makes a change on the same line that you did, it will ask you to merge
+#when you open the file it will show an error where there's something to be resolved
+#then save, git add // git commit
+
+##WORK FLOW
+
+#git pull to get all changes made by team
+#make changes
+# git add -A #to make all changes you've made
+# git commit -m "what I did"
+
+#to get out of a commit
+# esc : wq
